da0d7530c7d1d4be21683a09acf73136110306e3
Ruis MacDonald <rs660553@dal.ca>
2025-10-10 14:42:42 -0300

    dmcontrol_suite changes to add ReacherLoCA Domain

--- dm_control/suite/__init__.py
+++ dm_control/suite/__init__.py
@@ -40,6 +40,7 @@ from dm_control.suite import reacher
 from dm_control.suite import stacker
 from dm_control.suite import swimmer
 from dm_control.suite import walker
+from dm_control.suite import reacherloca
 
 # Find all domains imported.
 _DOMAINS = {name: module for name, module in locals().items()
